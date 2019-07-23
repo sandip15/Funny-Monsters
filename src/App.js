983a9df87f9a9ed9a0bdd8e4bd2346e11ea7fd07
sandip15 <sandiproy10@yahoo.com>
2019-07-23 15:43:40 +0800

    adding react app

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React,{ Component } from 'react';
+
+import './App.css';
+import {CardList} from './components/card-list/card-list.component';
+import {SearchBox} from './components/search-box/search-box'
+
+class App extends Component {
+    constructor(){
+      super();
+      this.state = {
+        monsters: [],
+        searchField :''
+      }
+    };
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({monsters : users}))
+
+  }
+
+    render(){
+      const {monsters, searchField} = this.state; 
+      const filteredMonsters = monsters.filter(monster => 
+        monster.name.toLowerCase().includes(searchField.toLowerCase()))
+      return(
+        <div className="App">
+          <h1> Funny Monsters </h1>
+          <SearchBox
+            placeholder ='search monsters' 
+            handleChange ={event => this.setState({searchField: event.target.value})}
+          />
+        <CardList monsters = {filteredMonsters} />
+        
+        </div>
+      );
+    }
+}
+
+export default App;
