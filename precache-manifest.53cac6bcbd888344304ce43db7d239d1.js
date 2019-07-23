7c6771fb1a3efff4b1464da865c5bee7336189be
sandip15 <sandiproy10@yahoo.com>
2019-07-23 16:19:28 +0800

    Updates

---
+++ precache-manifest.53cac6bcbd888344304ce43db7d239d1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3482656a778a9f122f4bb5b715790224",
+    "url": "/Funny-Monsters/index.html"
+  },
+  {
+    "revision": "8c9fd9994f947f5a15f8",
+    "url": "/Funny-Monsters/static/css/main.3b41d83a.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/Funny-Monsters/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "8c9fd9994f947f5a15f8",
+    "url": "/Funny-Monsters/static/js/main.e1794be6.chunk.js"
+  },
+  {
+    "revision": "794fa249d50ebd0cddf0",
+    "url": "/Funny-Monsters/static/js/runtime~main.649906c2.js"
+  }
+]);
